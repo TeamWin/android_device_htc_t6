134424a1e18e1336387d731fc086ff7a50f69992
David Hays <Dhays90@gmail.com>
2014-12-19 12:42:44 -0600

    First Commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+**TWRP - HTC One Max (International/GSM)**
+
+This is a recovery-only device tree for the HTC One Max for Internation/GSM (t6ul / t6).
+
+**Quick build instructions:**
+
+    . build/envsetup.sh
+    lunch cm_t6-eng
+    make recoveryimage
---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := t6
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/t6/device.mk)
+
+# Device identifier - this must come after all inclusions
+PRODUCT_DEVICE := t6
+PRODUCT_NAME := cm_t6
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_europe
